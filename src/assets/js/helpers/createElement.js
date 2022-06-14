9b7883cdd185cbb384ce33f5d53bce2784578466
HanningtoneM <hanningtonepompyy@gmail.com>
2022-06-14 15:13:36 +0300

    Add folder helpers to get and create  element

---
+++ src/assets/js/helpers/createElement.js
@@ -0,0 +1,9 @@
+export default (tag, attributes = {}, textContent = '') => {
+  const element = document.createElement(tag);
+  const keys = Object.keys(attributes);
+  keys.forEach((key) => {
+    element.setAttribute(key, attributes[key]);
+  });
+  element.textContent = textContent;
+  return element;
+};
