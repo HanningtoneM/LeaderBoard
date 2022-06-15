af7b573e1f06da3780ae12a340734cb1676aa60c
HanningtoneM <hanningtonepompyy@gmail.com>
2022-06-15 12:09:32 +0300

    Add leaderboard api

--- src/assets/js/view.js
+++ src/assets/js/view.js
@@ -6,23 +6,15 @@ export default () => {
   const scoresDiv = createElement('div', { class: 'scores-div' });
   const scoresDivHeader = createElement('div', { class: 'd-flex scores-div-header' });
   const scoresDivHeading = createElement('h3', {}, 'Resent Scores');
-  const refreshBtn = createElement('button', {}, 'Refresh');
+  const refreshBtn = createElement('button', { class: 'refresh-button' }, 'Refresh');
   const scoreList = createElement('ul', { class: 'score-list d-flex full-width' });
-  scoreList.innerHTML = `
-    <li>Name: 100</li>
-    <li>Name: 20</li>
-    <li>Name: 50</li>
-    <li>Name: 78</li>
-    <li>Name: 125</li>
-    <li>Name: 77</li>
-    <li>Name: 42</li>
-  `;
+
   scoresDivHeader.append(scoresDivHeading, refreshBtn);
   scoresDiv.append(scoresDivHeader, scoreList);
 
   const formDiv = createElement('div', { class: 'form-div' });
   const formHeading = createElement('h3', { class: 'full-width' }, 'Add your score');
-  const form = createElement('form', { class: 'd-flex' });
+  const form = createElement('form', { class: 'd-flex submit-score' });
   const nameInput = createElement('input', {
     type: 'text', placeholder: 'Your name', id: 'player_name', name: 'player_name',
   });
